51d4178b07563561c9174d32abf3eb54bbf15216
Smart_Yanmeng <York.Yanmeng@gmail.com>
2024-03-16 13:21:32 +0800

    2024/03/16
    2024/03/16

---
+++ cloud-parent/clazz/src/main/java/org/example/clazz/controller/MqSendController.java
@@ -0,0 +1,29 @@
+package org.example.clazz.controller;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.example.vo.ClazzVo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/mqSend")
+public class MqSendController {
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    @GetMapping("/sendMsg/{msg}")
+    public String sendMsg(@PathVariable String msg) {
+
+        ClazzVo clazzVo = new ClazzVo();
+        clazzVo.setClazzNo("1001班");
+        clazzVo.setId("001");
+        rocketMQTemplate.convertAndSend("student", clazzVo);
+
+        return "send success";
+    }
+}
---
+++ cloud-parent/student/src/main/java/org/example/student/service/OrderService.java
@@ -0,0 +1,15 @@
+package org.example.student.service;
+
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RocketMQMessageListener(consumerGroup = "stu2", topic = "student")
+public class OrderService implements RocketMQListener<String> {
+
+    @Override
+    public void onMessage(String s) {
+        System.out.println("OrderService接收到消息：" + s);
+    }
+}
