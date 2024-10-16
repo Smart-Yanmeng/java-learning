57db1dfa664724d5dad867b3487c49e0f0bcad86
Smart_Yanmeng <York.Yanmeng@gmail.com>
2024-10-16 17:33:55 +0800

    2024/10/16

---
+++ juc/src/main/java/com/York/Main.java
@@ -0,0 +1,29 @@
+package com.York;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(
+                () -> {
+                    int i = 0;
+
+                    for (int j = 0; j < 100; j++) {
+                        System.out.println("A -> " + ++i);
+                    }
+                }
+        );
+
+        Thread t2 = new Thread(
+                () -> {
+                    int i = 0;
+
+                    for (int j = 0; j < 100; j++) {
+                        System.out.println("B -> " + ++i);
+                    }
+                }
+        );
+
+        t1.start();
+        t2.start();
+    }
+}
