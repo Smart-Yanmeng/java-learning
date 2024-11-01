41423081d29243d5a27953968a372f56be388382
Smart_Yanmeng <York.Yanmeng@gmail.com>
2024-11-01 12:58:35 +0800

    2024/10/30

--- tally-book/src/Main.java
+++ tally-book/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         Integer principal = 2310;
         Double ratePerMonth = 0.02;
-        Integer days = 15;
+        Integer days = 16;
 
         System.out.println("The principal and interest is: " + calculateMoneyByDay(principal, ratePerMonth, days));
     }
